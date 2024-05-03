04154b071f0fefb014e580a63d30c576e3690551
Victor (Vicvenpet) Petkov <74773810+vicvenpet@users.noreply.github.com>
2024-05-04 00:40:53 +0200

    fix: 404.js deploy error

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,12 @@
+// pages/404.js
+
 import Link from 'next/link';
 
 export default function Custom404() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold text-gray-800 mb-4">Oops! Page Not Found</h1>
-      <p className="text-lg text-gray-600 mb-8">The page you're looking for does not exist or has been moved.</p>
+      <p className="text-lg text-gray-600 mb-8">The page you&apos;re looking for does not exist or has been moved.</p>
       <Link href="/">
         <a className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors duration-300">Go back to Home</a>
       </Link>
