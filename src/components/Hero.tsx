348ba24573bac72eb223b5fb46d8ac08b1087807
Victor (Vicvenpet) Petkov <74773810+vicvenpet@users.noreply.github.com>
2024-10-31 01:40:46 +0100

    fix finally i can sleep now 1:40 am

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Download, CalendarSearch } from 'lucide-react';
 
 export default function Hero() {
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { ExternalLink } from 'lucide-react';
+import { useState } from 'react';
 
 interface Project {
   id: number;
