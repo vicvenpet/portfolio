77f7f73cf08d4de164d9554ef1cfa991aec801d1
Victor (Vicvenpet) Petkov <74773810+vicvenpet@users.noreply.github.com>
2024-11-01 00:56:04 +0100

    dadaddaddaddddddddddd

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -12,7 +12,7 @@ interface ExperienceItemProps {
 
 const ExperienceItem: React.FC<ExperienceItemProps> = ({ title, company, description, link, date }) => {
   return (
-    <div className="relative mx-12 pb-12 grid before:absolute before:left-[-35px] before:block before:h-full before:border-l-2 before:border-black/20 dark:before:border-white/15 before:content-[''] md:grid-cols-5 md:gap-10 md:space-x-4">
+    <div className="relative max-w-4xl lg:mx-auto mx-12 pb-12 grid before:absolute before:left-[-35px] before:block before:h-full before:border-l-2 before:border-black/20 dark:before:border-white/15 before:content-[''] md:grid-cols-5 md:gap-10 md:space-x-4">
       <div className="relative pb-12 md:col-span-2">
         <div className="sticky top-0">
           <span className="text-blue-400 -left-[44px] absolute rounded-full text-5xl">&bull;</span>
